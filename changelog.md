60918d468a1fe882bf0cf2033292e8eb642dd73b
mgarces <manugp1994@gmail.com>
2025-08-08 10:58:11 +0200

    Video135

--- changelog.md
+++ changelog.md
@@ -8,3 +8,13 @@
 ## Video 131
 
 - npm i @nestjs/microservices nats dotenv joi
+
+## Video 133
+
+- npm i class-validator class-transformer
+
+## Video 135
+
+- npm i prisma --save-dev
+- npx prisma init # Esto crea un .env con DATA_URL el cual debemos configurar con la url de Mongo para crear el schema. Y en el archivo de prisma configurar provider = "mongodb" y crear el model "User"
+- npx prisma generate
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
@@ -13,6 +14,13 @@ async function bootstrap() {
       },
     },
   );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen();
 }
 bootstrap();
